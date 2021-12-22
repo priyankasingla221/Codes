e192e6e4ab138273819b13df20c41a525185dc82
Priyanka <priyankasingla221@gmail.com>
2021-12-21 22:57:33 -0400

    code

--- src/assignment2/IncrementOperator.java
+++ src/assignment2/IncrementOperator.java
@@ -3,9 +3,16 @@ package assignment2;
 public class IncrementOperator {
 
 	public static void main(String[] args) {
-		int x = 1;
+		int x = 2;
 		x = x++ + ++x + x++ + ++x + ++x;
 		System.out.println("Value of x : " + x);
+		
+		if (x % 2 == 0) {
+			System.out.println("Value of x is Even");
+
+		} else {
+			System.out.println("Value of x Odd");
+		}
 
 		boolean isEven= (x%2==0);
 		System.out.println("x value is even ? = " +isEven);
